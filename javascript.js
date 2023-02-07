95fb5940df89721e2b2f081d7dfd3f123bec7afd
CameronTheDeveloper <cameronposey@outlook.com>
2023-02-07 00:18:45 -0800

    Add clear button
    The getGridSize is a work in progress

--- javascript.js
+++ javascript.js
@@ -1,10 +1,27 @@
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 const grid = document.querySelector('#grid');
 
+const sizeBtn = document.querySelector('#sizeBtn');
+const clearBtn = document.querySelector('#clearBtn');
+sizeBtn.textContent = "Click here to enter grid size";
+clearBtn.textContent = "Clear";
 
-function getGridSize(){
+let gridHeight = 64;
+let gridWidth = 64;
+
+sizeBtn.addEventListener('click', () =>{
     let gridHeight = prompt("Enter grid height: ");
     let gridWidth = prompt("Enter grid width: ");
+    getGridSize(gridHeight, gridWidth);
+})
+
+clearBtn.addEventListener('click', () => {
+    clearGrid();
+    getGridSize(64, 64)
+})
+
+//User enters grid height and width
+function getGridSize(gridHeight, gridWidth){
     for(let i = 0; i < gridWidth; i++){
         let row = document.createElement('div');
         row.className = "row";
@@ -20,7 +37,14 @@ function getGridSize(){
     }
 }
 
-getGridSize();
+function clearGrid(){
+    grid.innerHTML = '';        //Clears all the html within grid
+}
+
+
+getGridSize(gridHeight, gridWidth);
+
+//getGridSize(gridHeight, gridWidth);
 
 
 //To make grid: Use double for loop, addEventListener for click/mouse button up
